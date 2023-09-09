b684fc0ce58688a9e8ceb53449a1a499807d2149
George Muscat <georgemuscat@proton.me>
2023-09-09 22:17:49 +1000

    more shadowing

--- shadowing/src/main.rs
+++ shadowing/src/main.rs
@@ -13,4 +13,9 @@ fn main() {
     println!("{}", letters);
     letters = "abcdef";
     println!("{}", letters);
+
+    let flt: f64 = 0.2;
+    println!("{}", flt);
+    let flt: f64 = flt + 0.1;
+    println!("{}", flt);
 }

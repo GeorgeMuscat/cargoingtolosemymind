38c4a0314658115a279000b718ce4a82897ad8e7
George Muscat <georgemuscat@proton.me>
2023-09-09 16:18:28 +1000

    hello cargo :

---
+++ hello_cargo/target/debug/deps/hello_cargo-fb5a1812c47eae7e.d
@@ -0,0 +1,5 @@
+/home/musca/cargoingtolosemymind/hello_cargo/target/debug/deps/hello_cargo-fb5a1812c47eae7e: src/main.rs
+
+/home/musca/cargoingtolosemymind/hello_cargo/target/debug/deps/hello_cargo-fb5a1812c47eae7e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/musca/cargoingtolosemymind/hello_cargo/target/debug/hello_cargo: /home/musca/cargoingtolosemymind/hello_cargo/src/main.rs
